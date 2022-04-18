093c4405b5ed812cb14eb93baa37de6da8357307
Alexandre Luís <alexandreluisrigotti@gmail.com>
2022-04-18 20:21:24 -0300

    Adiçãode stashe da primeira classe

---
+++ Pessoa.java
@@ -0,0 +1,15 @@
+Public class Pessoa()
+{
+    private String nome;
+    private int anoNascimento;
+
+    public String getNome()
+    {
+        return this.nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = n;
+    }
+}
